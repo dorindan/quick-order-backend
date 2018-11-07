cc011ee46052e21f8aebe3908fe349ec76f5cd1e
Dorin Gabudean <dorin.gabudean@ebs.msft>
2018-11-07 12:44:23 +0200

    first commit

---
+++ src/main/java/ro/quickorder/backend/QuickOrderBackendApplication.java
@@ -0,0 +1,12 @@
+package ro.quickorder.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickOrderBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuickOrderBackendApplication.class, args);
+    }
+}
